31fd97f5d38af16d32642d27eddd4ec12cef4c05
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-15 13:35:32 +0800

    打包成ES Module

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BaseEditor} from '../../dist/main.js'
 
 function App() {
+
+  useEffect(() => {
+    const editor = new BaseEditor()
+  }, []);
 
   return (
     <>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin')
+// 抽离所有的第三方模块
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: './src/index.ts',
@@ -7,7 +9,12 @@ module.exports = {
     devtool: false,
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        // 这样就可以scripts引入 也可以require或者import引入
+        library: {
+            name: "aurora_pen",
+            type: "ES Module",
+        },
     },
     module: {
         rules: [
@@ -20,5 +27,6 @@ module.exports = {
             },
         ]
     },
-    plugins: [new ESLintPlugin({ extensions: ['.js', '.ts'] })]
+    // plugins: [new ESLintPlugin({ extensions: ['.js', '.ts'] })],
+    externals: [nodeExternals()],
 }

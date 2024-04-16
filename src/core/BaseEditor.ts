98d31c979b66b7f6283acf8fe1b1d029898a097e
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-17 00:03:19 +0800

    add

--- src/core/BaseEditor.ts
+++ src/core/BaseEditor.ts
@@ -1,9 +1,11 @@
-import {schema} from "prosemirror-schema-basic"
+// import {schema} from "prosemirror-schema-basic"
 import {EditorState} from "prosemirror-state"
 import {EditorView} from "prosemirror-view"
 import {undo, redo, history} from "prosemirror-history"
 import {keymap} from "prosemirror-keymap"
 import {baseKeymap} from "prosemirror-commands"
+import {schema} from "./Schema"
+import { Schema, Node, DOMParser } from "prosemirror-model";
 
 class BaseEditor {
 	constructor(domId?: string) {
@@ -16,6 +18,10 @@ class BaseEditor {
 			plugins: [history(), keymap({"Mod-z": undo, "Mod-y": redo}), keymap(baseKeymap)]
 		})
 		let view = new EditorView(dom, {state});
+		
+	}
+	
+	private createBaseDoc() {
 		
 	}
 }
